b48c9dfca1a261765f563b49b4a9d231a317af1d
Victor Santos <victorsbarroso1@gmail.com>
2023-12-22 10:51:34 -0300

    Estilização ~ CSS

--- Branas/main.js
+++ Branas/main.js
@@ -1,52 +1,33 @@
 
-const janeiro = new mes("janeiro");
-janeiro.adicionarLancamento(new Lancamento("Salário", "receita", 3000));
-janeiro.adicionarLancamento(new Lancamento("Aluguel", "despesa", 1000));
-janeiro.adicionarLancamento(new Lancamento("Conta de Luz", "despesa", 200));
-janeiro.adicionarLancamento(new Lancamento("Conta de Água", "despesa", 100));
-janeiro.adicionarLancamento(new Lancamento("Internet", "despesa", 100));
-janeiro.adicionarLancamento(new Lancamento("Transporte", "despesa", 300));
-janeiro.adicionarLancamento(new Lancamento("Lazer", "despesa", 300));
-janeiro.adicionarLancamento(new Lancamento("Alimentação", "despesa", 500));
-janeiro.adicionarLancamento(new Lancamento("Condomínio", "despesa", 300));
-janeiro.adicionarLancamento(new Lancamento("Farmácia", "despesa", 100));
-
-
-const fevereiro = new mes("fevereiro",);
-fevereiro.adicionarLancamento(new Lancamento("Salário", "receita", 3000));
-fevereiro.adicionarLancamento(new Lancamento("Aluguel", "despesa", 1200));
-fevereiro.adicionarLancamento(new Lancamento("Conta de Luz", "despesa", 250));
-fevereiro.adicionarLancamento(new Lancamento("Conta de Água", "despesa", 100));
-fevereiro.adicionarLancamento(new Lancamento("Internet", "despesa", 100));
-fevereiro.adicionarLancamento(new Lancamento("Transporte", "despesa", 500));
-fevereiro.adicionarLancamento(new Lancamento("Alimentação", "despesa", 1000));
-fevereiro.adicionarLancamento(new Lancamento("Condomínio", "despesa", 400));
-
-const marco = new mes("marco",);
-marco.adicionarLancamento(new Lancamento("Salário", "receita", 4000));
-marco.adicionarLancamento(new Lancamento("Aluguel", "despesa", 1200));
-marco.adicionarLancamento(new Lancamento("Conta de Luz", "despesa", 200));
-marco.adicionarLancamento(new Lancamento("Conta de Água", "despesa", 100));
-marco.adicionarLancamento(new Lancamento("Internet", "despesa", 200));
-marco.adicionarLancamento(new Lancamento("Transporte", "despesa", 500));
-marco.adicionarLancamento(new Lancamento("Lazer", "despesa", 800));
-marco.adicionarLancamento(new Lancamento("Alimentação", "despesa", 1000));
-marco.adicionarLancamento(new Lancamento("Condomínio", "despesa", 400));
-
-const abril = new mes("abril");
-abril.adicionarLancamento(new Lancamento("Salário", "receita", 4000));
-
-
+const janeiro = new mes("Janeiro");
+janeiro.adicionarLancamento(new Lancamento("Salário", "Receita", 3000));
+janeiro.adicionarLancamento(new Lancamento("Aluguel", "Despesa", 1000));
+janeiro.adicionarLancamento(new Lancamento("Conta de Luz", "Despesa", 200));
+janeiro.adicionarLancamento(new Lancamento("Conta de Água", "Despesa", 100));
+const fevereiro = new mes("Fevereiro",);
+fevereiro.adicionarLancamento(new Lancamento("Salário", "Receita", 3000));
+fevereiro.adicionarLancamento(new Lancamento("Aluguel", "Despesa", 1200));
+fevereiro.adicionarLancamento(new Lancamento("Conta de Luz", "Despesa", 250));
+fevereiro.adicionarLancamento(new Lancamento("Conta de Água", "Despesa", 100));
+const marco = new mes("Março",);
+marco.adicionarLancamento(new Lancamento("Salário", "Receita", 4000));
+marco.adicionarLancamento(new Lancamento("Aluguel", "Despesa", 1200));
+marco.adicionarLancamento(new Lancamento("Conta de Luz", "Despesa", 200));
+marco.adicionarLancamento(new Lancamento("Conta de Água", "Despesa", 100));
+const abril = new mes("Abril");
+abril.adicionarLancamento(new Lancamento("Salário", "Receita", 4000));
+abril.adicionarLancamento(new Lancamento("Aluguel", "Despesa", 1200));
+abril.adicionarLancamento(new Lancamento("Conta de Luz", "Despesa", 200));
+abril.adicionarLancamento(new Lancamento("Conta de Água", "Despesa", 100));
 const ano = new Ano();
 ano.adicionarMes(janeiro);
 ano.adicionarMes(fevereiro);
 ano.adicionarMes(marco);
 ano.adicionarMes(abril);
 ano.calcularSaldo();
-
-janeiro.adicionarLancamento(new Lancamento("Escola", "despesa", 500));
-fevereiro.adicionarLancamento(new Lancamento("Escola", "despesa", 400));
-marco.adicionarLancamento(new Lancamento("Escola", "despesa", 500));
+janeiro.adicionarLancamento(new Lancamento("Escola", "Despesa", 500));
+fevereiro.adicionarLancamento(new Lancamento("Escola", "Despesa", 400));
+marco.adicionarLancamento(new Lancamento("Escola", "Despesa", 500));
 ano.calcularSaldo();
 
 console.log(ano.meses);
@@ -62,44 +43,78 @@ function addElement(parent, elementType, text) {
     parent.appendChild(element);
 }
 
-function renderizar() {
-
-    const app = document.getElementById ("app");
+function renderizar () {
+    const app = document.getElementById("app");
     if (app.firstChild) {
-        app.firstChild.remove ();
+        app.firstChild.remove();
+    }
+    const painel = document.createElement("div");
+    
+    const cores = ["red", "yellow", "green", "blue"]
+    const grafico = document.createElement("div");
+    grafico.className = "grafico";
+    for (const mes of ano.meses) {
+        const coluna = document.createElement("div");
+        coluna.classname = "grafico-coluna";
+        const cor = document.createElement("div");
+        cor.style.height = (mes.totalizador.saldo * 100) / 10000 + "px";
+        cor.style.background = cores.pop();
+        coluna.appendChild(cor);
+        const nomeDoMes = document.createElement("div");
+        nomeDoMes.className = "grafico-coluna-texto";
+        nomeDoMes.innerText = mes.nome;
+        coluna.appendChild(cor);
+        coluna.appendChild(nomeDoMes);
+        grafico.appendChild(coluna);
+    }
+    painel.appendChild(grafico);
+
+    for (const mes of ano.meses) {
+        addElement(painel, "h4", mes.nome);
+
+        const tabelaLancamentos = new Tabela ("tabela-lancamentos");        
+        tabelaLancamentos.addRow("th",["Categoria","Valor"]);
+    
+
+        for (const lancamento of mes.lancamentos) {
+
+            tabelaLancamentos.addRow("td",[lancamento.categoria, formatarDinheiro(lancamento.getValorString())])
+        }
+
+        tabelaLancamentos.addRow ("th",["Juros", formatarDinheiro(mes.totalizador.juros)]);
+        tabelaLancamentos.addRow ("th",["Rendimentos", formatarDinheiro(mes.totalizador.rendimentos)]);
+        tabelaLancamentos.addRow ("th",["Total", formatarDinheiro(mes.totalizador.saldo)]);
+        painel.appendChild(tabelaLancamentos.element);
 
     }
-    const painel = document.createElement ("div");
-    
-    for (const mes of ano.meses) {
-        addElement(painel, "h3", mes.nome);
-        for (const lancamento of mes.lancamentos) {
-            const detalhesLancamento = lancamento.tipo + " " + lancamento.categoria + " " + lancamento.valor;
-            addElement (painel, "p", detalhesLancamento);
-        }
-        addElement (painel, "h4", mes.totalizador.saldo);
-        addElement (painel, "hr");    
-    }
-    app.appendChild (painel);
+    app.appendChild(painel);
 }
 
 renderizar();
 
-function adicionarLancamento (){
+function adicionarLancamento() {
     const mes = document.getElementById("mes");
     const categoria = document.getElementById("categoria");
     const tipo = document.getElementById("tipo");
     const valor = document.getElementById("valor");
-    ano.adicionarLancamento(mes.value, new Lancamento (categoria.value, tipo.value, parseFloat(valor.value))); 
+    ano.adicionarLancamento(mes.value, new Lancamento(categoria.value, tipo.value, parseFloat(valor.value)));
     ano.calcularSaldo();
     renderizar();
-    mes.value = "";
+    mes.value = ano.meses[0].nome;
     categoria.value = "";
-    tipo.value = "";
+    tipo.value = "Receita";
     valor.value = "";
 }
 
-const botao = document.getElementById ("botao");
-botao.addEventListener ("click", adicionarLancamento);
+const botao = document.getElementById("botao");
+botao.addEventListener("click", adicionarLancamento);
+
+const mesSelect = document.getElementById("mes");
+
+for (const mes of ano.meses) {
+    const option = document.createElement("option");
+    option.text = mes.nome;
+    mesSelect.add(option);
+}
 
 
--- Branas/mes.js
+++ Branas/mes.js
@@ -1,10 +1,9 @@
 class mes {
 
-	constructor (nome, ) {
-		if (nome === "") throw new Error("Mês Inválido: O nome é obrigatório");
+	constructor (nome) {
 		this.nome = nome;
 		this.saldoInicial = 0;
-		this.totalizador = { saldo: 0, juros: 0, rendimentos: 0, receitas: 0, despesas: 0, distribuicaoDeDespesas: [] };
+		this.totalizador = { saldo: 0, juros: 0, rendimentos: 0, Receitas: 0, Despesas: 0, distribuicaoDeDespesas: [] };
 		this.lancamentos = [];
 	}
 
@@ -13,7 +12,7 @@ class mes {
 	}
 
 	calcularSaldo () {
-		this.totalizador = { saldo: 0, juros: 0, rendimentos: 0, receitas: 0, despesas: 0, distribuicaoDeDespesas: [] };
+		this.totalizador = { saldo: 0, juros: 0, rendimentos: 0, Receitas: 0, Despesas: 0, distribuicaoDeDespesas: [] };
 		this.totalizador.saldo = this.saldoInicial;
 		this.apurarReceitas();
 		this.apurarDespesas();
@@ -24,18 +23,18 @@ class mes {
 
 	apurarReceitas () {
 		for (const lancamento of this.lancamentos) {
-			if (lancamento.tipo === "receita") {
+			if (lancamento.tipo === "Receita") {
 				this.totalizador.saldo += lancamento.valor;
-				this.totalizador.receitas += lancamento.valor;
+				this.totalizador.Receitas += lancamento.valor;
 			}
 		}
 	}
 
 	apurarDespesas () {
 		for (const lancamento of this.lancamentos) {
-			if (lancamento.tipo === "despesa") {
+			if (lancamento.tipo === "Despesa") {
 				this.totalizador.saldo -= lancamento.valor;
-				this.totalizador.despesas += lancamento.valor;
+				this.totalizador.Despesas += lancamento.valor;
 			}
 		}
 	}
@@ -43,8 +42,8 @@ class mes {
 	distribuirDespesas () {
 		const distribuicaoDeDespesas = [];
 		for (const lancamento of this.lancamentos) {
-			if (lancamento.tipo === "despesa") {
-				const percentual = arredondar((lancamento.valor/this.totalizador.despesas)*100);
+			if (lancamento.tipo === "Despesa") {
+				const percentual = arredondar((lancamento.valor/this.totalizador.Despesas)*100);
 				distribuicaoDeDespesas.push({ categoria: lancamento.categoria, percentual });
 			}
 		}
